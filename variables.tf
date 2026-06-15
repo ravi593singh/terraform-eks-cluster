78212ecbb320e1847a38f2b94990a20b3b7dcb4e
Ravi Kumar Singh <112927773+ravi593singh@users.noreply.github.com>
2026-06-15 22:12:47 +0530

    Add dev and prod environments with corresponding .tfvars files and update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,47 +1,48 @@
 variable "region" {
   description = "AWS region where the EKS cluster will be deployed"
   type        = string
-  default     = "us-west-2"
 }
 
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "demo-eks-cluster"
 }
 
 variable "cluster_version" {
   description = "EKS Kubernetes version"
   type        = string
-  default     = "1.29"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "node_instance_type" {
   description = "EC2 instance type for worker nodes"
   type        = string
-  default     = "t3.medium"
 }
 
 variable "desired_size" {
   description = "Number of nodes for the managed node group"
   type        = number
-  default     = 2
 }
 
 variable "min_size" {
   description = "Minimum number of nodes in the managed node group"
   type        = number
-  default     = 2
 }
 
 variable "max_size" {
   description = "Maximum number of nodes in the managed node group"
   type        = number
-  default     = 2
+}
+
+variable "environment" {
+  description = "Environment name (dev or prod)"
+  type        = string
+  validation {
+    condition     = contains(["dev", "prod"], var.environment)
+    error_message = "Environment must be either 'dev' or 'prod'."
+  }
 }
